51ecf587f2905e857cd3a41b8908f37dbbcc6959
batable245 <iordan.kar2@gmail.com>
2019-10-27 09:01:34 +0200

    Added basics NestedConditionalExercise tasks

---
+++ cSharpBasics/repos/NestedConditionalsExercise/volleyball/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace volleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string year = Console.ReadLine();
+            int holidaysInYear = int.Parse(Console.ReadLine()); 
+            int weekendsHome = int.Parse(Console.ReadLine());
+
+            double gamesPlayedExtra = 0.0;
+
+            int weekends = 48;
+            double weekendsInSofia = weekends - weekendsHome;
+            double gamesInSofia = weekendsInSofia * 0.75;
+            double holidaysPlaying = holidaysInYear * 2.00 / 3;
+            double gamesPlayed = gamesInSofia + weekendsHome + holidaysPlaying;
+
+            //check logic again
+            if (year.Equals("leap"))
+            {
+                gamesPlayedExtra = gamesPlayed * 0.15;
+            }     
+
+            double gamesPlayedInYear = gamesPlayed + gamesPlayedExtra;
+
+            Console.WriteLine(Math.Floor(gamesPlayedInYear));
+        }
+    }
+}
